60f4a501cd46983751997c66df547543a80a4020
MarcYoba <yobamarc1@gmail.com>
2026-07-26 14:35:17 +0100

    creation des donne de la ferme liste , delete et edite

--- src/Form/FermesType.php
+++ src/Form/FermesType.php
@@ -6,6 +6,8 @@ use App\Entity\Fermes;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,15 +16,47 @@ class FermesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('localisation')
-            ->add('responsable')
-            ->add('telephone')
-            ->add('capacite')
-            ->add('nombreBatiments')
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
+            ->add('nom', TextType::class,[
+                'label' => 'Nom de la ferme',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom de la ferme',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('localisation', TextType::class,[
+                'label' => 'Localisation',
+                'attr' => [
+                    'placeholder' => 'Entrez la localisation',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('responsable', TextType::class,[
+                'label' => 'Responsable',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom du responsable',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('telephone', TextType::class,[
+                'label' => 'Téléphone',
+                'attr' => [
+                    'placeholder' => 'Entrez le numéro de téléphone',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('capacite', NumberType::class,[
+                'label' => 'Capacité',
+                'attr' => [
+                    'placeholder' => 'Entrez la capacité',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('nombreBatiments', NumberType::class,[
+                'label' => 'Nombre de bâtiments',
+                'attr' => [
+                    'placeholder' => 'Entrez le nombre de bâtiments',
+                    'class' => 'form-control',
+                ],
             ])
         ;
     }
